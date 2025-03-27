863b9e4ae03e5a01567a3395db311abc9330cf9a
JosueAVD27 <ajvelasquez4@espe.edu.ec>
2025-03-26 23:01:43 -0500

    feat: refactor route loading logic to improve readability and maintainability

--- backend/src/route.ts
+++ backend/src/route.ts
@@ -1,37 +1,41 @@
 import 'module-alias/register';
 import fs from 'fs';
-import path from "path";
-import { logRouteLoaded } from "@/src/monitor";
+import path from 'path';
+import { logRouteLoaded } from '@/src/monitor';
 import { Application } from 'express';
 
 // Escanear rutas de la API en la carpeta `src/`
 export function loadRoutes(app: Application, apiVersion: string): void {
     const srcPath = path.join(__dirname);
 
+    const isRouteFile = (file: string) =>
+        file.endsWith('.routes.ts') || file.endsWith('.routes.js');
+
+    const getRouteName = (file: string) =>
+        file.replace('.routes.ts', '').replace('.routes.js', '');
+
     const loadDirectoryRoutes = (basePath: string): void => {
         fs.readdirSync(basePath).forEach((file: string) => {
-            
             const fullPath = path.join(basePath, file);
 
             if (fs.statSync(fullPath).isDirectory()) {
                 loadDirectoryRoutes(fullPath); // Llamado recursivo a subdirectorios
-            } else if (file.endsWith('.routes.ts')) {
-                const routeName = file.replace('.routes.ts', '').replace('.routes.js', ''); // Obtiene el nombre del archivo sin extensión
-                const routePath = `${apiVersion}/${routeName}`; // Define la URL de la API
-
-                // Mensaje de carga de API en consola
+            } else if (isRouteFile(file)) {
+                const routeName = getRouteName(file);
+                const routePath = `${apiVersion}/${routeName}`;
                 logRouteLoaded(routePath);
 
-                // Import dinámico de rutas
-                import(fullPath).then((routeModule: any) => {
-                    app.use(routePath, routeModule.default);
-                }).catch((err) => {
-                    console.error(`Error al cargar la ruta ${fullPath}:`, err);
-                });
+                // Import dinámico
+                import(fullPath)
+                    .then((routeModule: any) => {
+                        app.use(routePath, routeModule.default);
+                    })
+                    .catch((err) => {
+                        console.error(`Error al cargar la ruta ${fullPath}:`, err);
+                    });
             }
         });
     };
 
-    // Detecta cualquier `.routes.ts` o `.routes.js`
     loadDirectoryRoutes(srcPath);
 }
